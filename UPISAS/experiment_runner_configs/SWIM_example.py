b3c1c543f0ec6626db8c68bd33b0a01ee5015739
Juliette Waltregny <juliettewaltregny@MacBook-Air-de-Juliette.local>
2024-11-20 16:09:53 +0100

    Experiment-runner files added

--- UPISAS/experiment_runner_configs/SWIM_example.py
+++ UPISAS/experiment_runner_configs/SWIM_example.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
+
 from EventManager.Models.RunnerEvents import RunnerEvents
 from EventManager.EventSubscriptionController import EventSubscriptionController
 from ConfigValidator.Config.Models.RunTableModel import RunTableModel
